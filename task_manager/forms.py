d903b3305a20b7f2cef73e868ac91227823543fa
DemExt <xldemlx@gmail.com>
2026-03-26 19:45:45 +0300

    step8

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -38,20 +38,36 @@ class CustomUserChangeForm(UserChangeForm):
     first_name = forms.CharField(label="Имя", required=True, max_length=150)
     last_name = forms.CharField(label="Фамилия", required=True, max_length=150)
 
+    password1 = forms.CharField(
+        label="Пароль",
+        widget=forms.PasswordInput,
+        required=False, # Сделаем необязательным, если пароль не меняется
+        help_text="Ваш пароль должен содержать не менее 3 символов."
+    )
+    password2 = forms.CharField(
+        label="Подтверждение пароля",
+        widget=forms.PasswordInput,
+        required=False
+    )
+
     class Meta:
         model = User
         fields = ("username", "first_name", "last_name")
-
-    class Meta(UserCreationForm.Meta):
-        model = User
-        # Порядок полей в форме
-        fields = ("first_name", "last_name", "username")
 
     def clean_password1(self):
         password = self.cleaned_data.get("password1")
         if password and len(password) < 3:
             raise ValidationError("Пароль должен содержать не менее 3 символов.")
         return password
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get("password1")
+        if password:
+            user.set_password(password) # Хэшируем пароль, если он введен
+        if commit:
+            user.save()
+        return user
 
 class LabelForm(forms.ModelForm):
     class Meta:
