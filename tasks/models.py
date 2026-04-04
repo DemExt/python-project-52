8147b45a24125984a5023f611843f273cac5423a
DemExt <xldemlx@gmail.com>
2026-04-04 23:02:27 +0300

    del3.14

--- tasks/models.py
+++ tasks/models.py
@@ -6,11 +6,11 @@ from labels.models import Label
 User = get_user_model()
 
 class Task(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name="Имя")
+    name = models.CharField(max_length=100, verbose_name="Имя")
     description = models.TextField(blank=True, verbose_name="Описание")
     status = models.ForeignKey(Status, on_delete=models.PROTECT, related_name="tasks", verbose_name="Статус")
     author = models.ForeignKey(User, on_delete=models.PROTECT, related_name="created_tasks", verbose_name="Автор")
-    executor = models.ForeignKey(User, on_delete=models.PROTECT, related_name="tasks", verbose_name="Исполнитель")
+    executor = models.ForeignKey(User, on_delete=models.PROTECT, related_name="tasks", null=True, blank=True, verbose_name="Исполнитель")
     labels = models.ManyToManyField(Label, related_name="tasks", blank=True, verbose_name="Метки")
     created_at = models.DateTimeField(auto_now_add=True)
 
